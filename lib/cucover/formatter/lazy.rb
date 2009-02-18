36a756759a361fb65f34d163fc719d863e37cd6b
Matt Wynne <matt@mattwynne.net>
2009-02-18 08:28:22 +0000

    Working on better output for dogfooding

--- lib/cucover/formatter/lazy.rb
+++ lib/cucover/formatter/lazy.rb
@@ -2,7 +2,49 @@ require 'cucumber'
 require 'rcov'
 
 module Cucover
+  
+  module FeaturesExtensions
+    def run_logs
+      @features.map{ |f| f.last_run }
+    end    
+  end
+  
+  module FeatureExtensions
+    attr_accessor :last_run
+    attr_accessor :analyzed_files
+  end
+  
+  module LooksLikeTerminal
+    def tty?
+      true
+    end
+  end
+  
   module Formatter
+    
+    class CoverageReporter
+      def initialize(io)
+        @io = io
+      end
+      
+      def visit_features(features)
+        @io.puts
+        @io.puts
+        @io.puts "Coverage"
+        @io.puts "--------"
+        @io.puts
+        features.accept(self)
+        @io.puts
+      end
+      
+      def visit_feature(feature)
+        @io.puts feature.file
+        feature.analyzed_files.each do |f|
+          @io.puts "  #{f}"
+        end
+      end
+    end
+    
     class Lazy < Cucumber::Ast::Visitor
 
       def initialize(step_mother, io, options)
@@ -10,23 +52,33 @@ module Cucover
         @io = io
         @options = options
         @analyzer = Rcov::CodeCoverageAnalyzer.new
+        @pretty_io = StringIO.new.extend(LooksLikeTerminal)
+        @pretty_formatter = Cucumber::Formatter::Pretty.new(step_mother, @pretty_io, @options)
       end
 
       def visit_features(features)
-        @features = features
+        features.extend(FeaturesExtensions)
         super
-        @io.puts
+        
+        CoverageReporter.new(@io).visit_features(features)
+
         print_counts(features)
+        @io.puts
       end
 
       def visit_feature(feature)
+        feature.extend(FeatureExtensions)
         @analyzer.run_hooked do
           super
         end
-        @io.puts feature.file
-        analyzed_files.each do |f|
-          @io.puts "  #{f}"
-        end
+        feature.analyzed_files = analyzed_files
+        
+        @pretty_formatter.visit_feature(feature)
+        feature.last_run = @pretty_io.string
+        @pretty_io.string == ""
+        @io.puts feature.last_run
+        @io.puts
+        
       end
       
       private 
@@ -57,7 +109,6 @@ module Cucover
       def dump_count(count, what, state=nil)
         [count, state, "#{what}#{count == 1 ? '' : 's'}"].compact.join(" ")
       end
-      
 
     end
   end
