0d2b0039e3a270bd782d6d73cbf7aae83fa3d66b
Joseph Wilk <joe@josephwilk.net>
2010-03-27 12:23:10 +0000

    Enable file logging

--- lib/cucover/logging_config.rb
+++ lib/cucover/logging_config.rb
@@ -1,16 +1,16 @@
 Logging.configure do
   logger('Cucover') do
     level     :debug
-    # appenders 'logfile'
+    appenders 'logfile'
   end
 
-  # appender('logfile') do
-  #   type      'File'
-  #   level     :debug
-  #   filename  File.dirname(__FILE__) + '/../../tmp/cucover.log'
-  #   layout do
-  #     type        'Pattern'
-  #     pattern     '[%d] %l  %c : %m\n'
-  #   end
-  # end  
+  appender('logfile') do
+     type      'File'
+     level     :debug
+     filename  File.dirname(__FILE__) + '/../../tmp/cucover.log'
+     layout do
+       type        'Pattern'
+       pattern     '[%d] %l  %c : %m\n'
+     end
+   end  
 end
