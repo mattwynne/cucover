64ad17fe744d5c9bae6db5d0f390b77daad028c8
Joseph Wilk <joe@josephwilk.net>
2010-03-28 11:11:09 +0100

    Hack to prevent state from bleeding between scenarios. Needs a nicer solution

--- lib/cucover/cucumber_hooks.rb
+++ lib/cucover/cucumber_hooks.rb
@@ -3,7 +3,7 @@ module Cucover
     def file_colon_line
       "#{file}:#{line}"
     end
-    
+
     def file
       @scenario_outline.file_colon_line.split(':').first
     end
@@ -12,10 +12,20 @@ end
 
 Cucover::Monkey.extend_every Cucumber::Ast::OutlineTable::ExampleRow => Cucover::ExampleRowExtensions
 
+# Fragile and Ugly
+# skip_invoke will skip all scenarios for a feature
+# Ensure we reset skipped status on steps.
+def reset_skipped_steps!(feature_element)
+  steps = feature_element.instance_variable_get("@steps")
+  steps.each { |step| step.instance_variable_set("@skip_invoke", nil) } if steps
+end
+
 Before do |scenario_or_table_row|
+  reset_skipped_steps!(scenario_or_table_row)
+
   Cucover.logger.info("Starting #{scenario_or_table_row.class} #{scenario_or_table_row.file_colon_line}")
   Cucover::Rails.patch_if_necessary
-  
+
   if Cucover.should_execute?(scenario_or_table_row)
     Cucover.start_recording!(scenario_or_table_row)
   else
